90985b070724bc2ed5382439f0a77b48296b65aa
Alexey Pisanko <apis@users.noreply.github.com>
2018-10-16 10:07:13 -0400

    Added ability to configure host and port through configuration

--- Ipc.Client.Implementation/IpcClientImplementation.cs
+++ Ipc.Client.Implementation/IpcClientImplementation.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+using System.Reflection;
 using System.Threading;
 using Grpc.Core;
 using log4net;
@@ -9,16 +12,26 @@ namespace Ipc.Client
 	{
 		private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-		protected internal const string HostName = "127.0.0.1";
-		protected internal const string PortName = "11972";
+		private const string HostKey = "Host";
+		private const string PortKey = "Port";
+		private string _host;
+		private int _port = -1;
 		private Channel _channel;
 		private CancellationTokenSource _cancellationTokenSource;
+
+		public IpcClientImplementation()
+		{
+			ParseConfiguration();
+
+			if (_port == -1 || _host == null)
+				throw new Exception("Invalid configuration!");
+		}
 
 		public void Start()
 		{
 			_cancellationTokenSource = new CancellationTokenSource();
-			_channel = new Channel(HostName + ":" + PortName, ChannelCredentials.Insecure);
-			Log.DebugFormat("Start(), Host: {0}, Port: {1}", HostName, PortName);
+			_channel = new Channel(_host + ":" + _port, ChannelCredentials.Insecure);
+			Log.DebugFormat("Start(), Host: {0}, Port: {1}", _host, _port);
 		}
 
 		public void Stop()
@@ -31,5 +44,43 @@ namespace Ipc.Client
 		public object Channel => _channel;
 
 		public CancellationToken CancellationToken => _cancellationTokenSource.Token;
+
+		private void ParseConfiguration()
+		{
+			var assemblyLocation = GetType().Assembly.Location;
+			var configuration = ConfigurationManager.OpenExeConfiguration(assemblyLocation);
+
+			var portKeyElement = configuration.AppSettings.Settings[PortKey];
+
+			if (portKeyElement == null)
+			{
+				Log.ErrorFormat("Configuration key for {0} can't be found", PortKey);
+				return;
+			}
+
+			if (!int.TryParse(portKeyElement.Value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var port))
+			{
+				Log.ErrorFormat("Configuration key for {0} can't be parsed", PortKey);
+				return;
+			}
+
+			if (port <= 0)
+			{
+				Log.ErrorFormat("Configuration key for {0} can't be negative", PortKey);
+				return;
+			}
+
+			_port = port;
+
+			var hostKeyElement = configuration.AppSettings.Settings[HostKey];
+
+			if (hostKeyElement == null)
+			{
+				Log.ErrorFormat("Configuration key for {0} can't be found", HostKey);
+				return;
+			}
+
+			_host = hostKeyElement.Value;
+		}
 	}
 }
--- Ipc.Server.Implementation/IpcServerImplementation.cs
+++ Ipc.Server.Implementation/IpcServerImplementation.cs
@@ -1,33 +1,102 @@
-﻿using AcquisitionManager;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using AcquisitionManager;
 using Grpc.Core;
 using Ipc.Definitions;
+using log4net;
 
 namespace Ipc.Server
 {
 	public class IpcServerImplementation : IIpcServer
 	{
-		private const int Port = 11972;
-		private Grpc.Core.Server _server;
+		private const string HostKey = "Host";
+		private const string PortKey = "Port";
+		private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 		private readonly IAcquisitionManager _acquisitionManager;
+		private readonly List<string> _hosts = new List<string>();
+		private int _port = -1;
+
+		private Grpc.Core.Server _server;
 
 		public IpcServerImplementation(IAcquisitionManager acquisitionManager)
 		{
 			_acquisitionManager = acquisitionManager;
+			ParseConfiguration();
+
+			if (_port == -1 || _hosts.Count == 0)
+				throw new Exception("Invalid configuration!");
 		}
 
 		public void Start()
 		{
 			_server = new Grpc.Core.Server
 			{
-				Services = {AcquisitionManagerService.BindService(new AcquisitionManagerServiceImplementation(_acquisitionManager))},
-				Ports = {new ServerPort("localhost", Port, ServerCredentials.Insecure)}
+				Services =
+				{
+					AcquisitionManagerService.BindService(
+						new AcquisitionManagerServiceImplementation(_acquisitionManager))
+				}
 			};
+
+			foreach (var host in _hosts)
+			{
+				var serverPort = new ServerPort(host, _port, ServerCredentials.Insecure);
+				_server.Ports.Add(serverPort);
+			}
+
 			_server.Start();
 		}
 
 		public void Stop()
 		{
 			_server.ShutdownAsync().Wait();
+		}
+
+		private void ParseConfiguration()
+		{
+			var assemblyLocation = GetType().Assembly.Location;
+			var configuration = ConfigurationManager.OpenExeConfiguration(assemblyLocation);
+
+			var portKeyElement = configuration.AppSettings.Settings[PortKey];
+
+			if (portKeyElement == null)
+			{
+				Log.ErrorFormat("Configuration key for {0} can't be found", PortKey);
+				return;
+			}
+
+			if (!int.TryParse(portKeyElement.Value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var port))
+			{
+				Log.ErrorFormat("Configuration key for {0} can't be parsed", PortKey);
+				return;
+			}
+
+			if (port <= 0)
+			{
+				Log.ErrorFormat("Configuration key for {0} can't be negative", PortKey);
+				return;
+			}
+
+			_port = port;
+
+			var hostKeys = configuration.AppSettings.Settings.AllKeys.Where(item => item.StartsWith(HostKey));
+
+			foreach (var hostKey in hostKeys)
+			{
+				var hostKeyElement = configuration.AppSettings.Settings[hostKey];
+
+				if (hostKeyElement == null)
+				{
+					Log.ErrorFormat("Configuration key for {0} can't be found", hostKey);
+					continue;
+				}
+
+				_hosts.Add(hostKeyElement.Value);
+			}
 		}
 	}
 }
